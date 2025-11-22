f68d24309fe047dd8ee0107697c012629e1f2e27
favainhat <103869364+favainhat@users.noreply.github.com>
2025-11-22 13:42:04 +0900

    Fix : ensure placeholder empty line always included even if cfg_skipempty is true

--- src/foo_alsong_lyric/LyricManager.cpp
+++ src/foo_alsong_lyric/LyricManager.cpp
@@ -213,7 +213,7 @@ std::vector<LyricLine> LyricManager::GetLyricBefore(int n)
 		std::vector<LyricLine>::const_iterator it = m_LyricLine - 1;
 		for(cnt = 0; (cnt < n); cnt ++, it --)
 		{
-			if((cfg_skipempty && boost::trim_copy(it->lyric).size() != 0) || !cfg_skipempty)
+			if((cfg_skipempty && boost::trim_copy(it->lyric).size() != 0) || m_CurrentLyric->IsBeginOfLyric(it) || !cfg_skipempty)
 				ret.push_back(*it);
 			else if(cfg_skipempty) //empty line
 				cnt --;
@@ -235,7 +235,7 @@ std::vector<LyricLine> LyricManager::GetLyric()
 		std::vector<LyricLine> ret;
 		std::vector<LyricLine>::const_iterator it;
 		for(it = m_LyricLine; !m_CurrentLyric->IsEndOfLyric(it) && it->time == m_LyricLine->time; it ++)
-			if((cfg_skipempty && boost::trim_copy(it->lyric).size() != 0) || !cfg_skipempty)
+			if((cfg_skipempty && boost::trim_copy(it->lyric).size() != 0) || m_CurrentLyric->IsBeginOfLyric(it) || !cfg_skipempty)
 				ret.push_back(*it);
 
 		return ret;
@@ -252,7 +252,7 @@ std::vector<LyricLine> LyricManager::GetLyricAfter(int n)
 		std::vector<LyricLine>::const_iterator it;
 		for(it = m_LyricLine + 1, cnt = 0; (cnt < n) && !m_CurrentLyric->IsEndOfLyric(it); cnt ++, it ++)
 		{
-			if((cfg_skipempty && boost::trim_copy(it->lyric).size() != 0) || !cfg_skipempty)
+			if((cfg_skipempty && boost::trim_copy(it->lyric).size() != 0) || m_CurrentLyric->IsBeginOfLyric(it) || !cfg_skipempty)
 				ret.push_back(*it);
 			else if(cfg_skipempty) //empty line
 				cnt --;
